824bfcc7b23df8439d9ba124290fea930d58b4d4
Student <example@example.com>
2019-10-28 15:14:09 -0700

    just messing around

---
+++ js/scripts-extraData.js
@@ -0,0 +1,10 @@
+$(document).reaady(function() {
+   $("#input").submit(function(event) {
+     event.preventDefault;
+
+      var toppings = ["pineapple", "coffee beans", "vanilla extract", "cheetos", "salad dressing", "chcolate chips"]
+
+      $("#output").text()
+
+   })
+})
